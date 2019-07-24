f6d6e953ccf9cfb52c60dcaa15719992400ef7b4
Monik Gandhi <gandhi.monik@gmail.com>
2019-07-24 07:37:27 -0700

    Added semantic-ui and semantic-ui-react

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './libstack-logo.jpg';
 import './App.css';
+import {Button, Grid, Input} from 'semantic-ui-react';
 
 function App() {
   return (
@@ -8,6 +9,16 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
       </header>
+      <Grid padded>
+        <Grid.Row centered>
+          <Grid.Column width={10}>
+            <Input fluid size='big' placeholder='Search...' />
+          </Grid.Column>
+          <Grid.Column width={3}>
+            <Button fluid color='orange' content="Search" size='big' />
+          </Grid.Column>
+        </Grid.Row>
+      </Grid>
     </div>
   );
 }
