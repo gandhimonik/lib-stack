79a1c73d84f7d528d1a0eb3d3767594eac640dd5
Monik Gandhi <gandhi.monik@gmail.com>
2019-10-31 07:50:16 -0700

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/lib-stack/precache-manifest.9548078922d1c8c9b44cad80b03ca3b5.js"
+  "/lib-stack/precache-manifest.9d8724515febc2f2002680e659e5bbfe.js"
 );
 
 self.addEventListener('message', (event) => {
